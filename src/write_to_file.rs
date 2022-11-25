de7db3b1ca7ff98f329d20d89b966b8b76710e6f
an-Iceberg <a.g.marosi@bluewin.ch>
2022-11-25 21:38:57 +0100

    -

--- src/write_to_file.rs
+++ src/write_to_file.rs
@@ -6,7 +6,13 @@ pub fn run()
 
   let mut file = File::create("output.txt").expect("Could not create file");
 
-  file.write_all(b"Welcome to Rust").expect("Could not write to file");
+  file.write_all(b"Welcome to Rust\n").expect("Could not write to file");
+
+  match file.write(b"Hello")
+  {
+    Ok(_) => {},
+    Err(error) => println!("{}", error)
+  }
 
   println!();
 }
